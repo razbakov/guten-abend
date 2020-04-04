66b5982186f645e5c46b6a8b00793f50f020f4df
Aleksey Razbakov <aleksey@razbakov.com>
2020-04-04 23:57:44 +0200

    ordered players list

--- components/Mafia/Moderator.vue
+++ components/Mafia/Moderator.vue
@@ -19,17 +19,47 @@
           {{ playerIds.length }}.
         </div>
       </div>
-      <div
-        v-for="(player, playerId) in game.players"
-        :key="playerId"
-        class="flex items-center"
-      >
-        <div v-if="game.places" class="bg-gray-200 w-6 h-6 mr-2 text-center">
-          {{ game.places[playerId] }}
+      <div class="mt-4">
+        <h2 class="text-xl font-bold">Players</h2>
+        <div
+          v-for="player in players"
+          :key="player.id"
+          class="flex items-center"
+        >
+          <div v-if="player.place" class="bg-gray-200 w-6 h-6 mr-2 text-center">
+            {{ player.place }}
+          </div>
+          <div>{{ player.nickname }}</div>
+          <div v-if="player.role" class="ml-2 text-center">
+            - {{ player.role }}
+          </div>
         </div>
-        <div>{{ player.nickname }}</div>
-        <div v-if="game.roles" class="ml-2 text-center">
-          - {{ game.roles[playerId] }}
+      </div>
+      <div class="mt-4">
+        <h2 class="text-xl font-bold">Game Set</h2>
+        <div class="typo">
+          <table>
+            <tr>
+              <td>Citizens</td>
+              <td>{{ game.set.roles.citizen }}</td>
+            </tr>
+            <tr>
+              <td>Mafia</td>
+              <td>{{ game.set.roles.mafia }}</td>
+            </tr>
+            <tr>
+              <td>Don</td>
+              <td>{{ game.set.roles.don }}</td>
+            </tr>
+            <tr>
+              <td>Sheriff</td>
+              <td>{{ game.set.roles.sheriff }}</td>
+            </tr>
+            <tr class="font-bold">
+              <td>Total</td>
+              <td>{{ game.set.count }}</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
@@ -40,6 +70,7 @@
 import useLiveDoc from '~/use/liveDoc'
 import useRouter from '~/use/router'
 import TLoader from '~/components/TLoader'
+import { sortBy } from '~/utils'
 
 export default {
   components: {
@@ -61,6 +92,16 @@ export default {
   computed: {
     playerIds() {
       return Object.keys(this.game.players)
+    },
+    players() {
+      return Object.keys(this.game.players)
+        .map((playerId) => ({
+          id: playerId,
+          nickname: this.game.players[playerId].nickname,
+          place: this.game.places[playerId],
+          role: this.game.roles[playerId]
+        }))
+        .sort(sortBy('place'))
     }
   },
   methods: {
--- components/Mafia/Player.vue
+++ components/Mafia/Player.vue
@@ -6,8 +6,52 @@
         <h1 class="text-3xl font-bold">{{ profile.nickname }}</h1>
       </div>
       <div>
-        <div v-if="game.places">Player Number: {{ game.places[uid] }}</div>
+        <div v-if="game.places">Place: {{ game.places[uid] }}</div>
         <div v-if="game.roles">Role: {{ game.roles[uid] }}</div>
+      </div>
+      <div class="mt-4">
+        <h2 class="text-xl font-bold">Players</h2>
+        <div
+          v-for="player in players"
+          :key="player.id"
+          class="flex items-center"
+          :class="uid === player.id ? 'font-bold' : ''"
+        >
+          <div v-if="player.place" class="bg-gray-200 w-6 h-6 mr-2 text-center">
+            {{ player.place }}
+          </div>
+          <div>{{ player.nickname }}</div>
+          <div v-if="player.role" class="ml-2 text-center">
+            - {{ player.role }}
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <h2 class="text-xl font-bold">Game Set</h2>
+        <div class="typo">
+          <table>
+            <tr>
+              <td>Citizens</td>
+              <td>{{ game.set.roles.citizen }}</td>
+            </tr>
+            <tr>
+              <td>Mafia</td>
+              <td>{{ game.set.roles.mafia }}</td>
+            </tr>
+            <tr>
+              <td>Don</td>
+              <td>{{ game.set.roles.don }}</td>
+            </tr>
+            <tr>
+              <td>Sheriff</td>
+              <td>{{ game.set.roles.sheriff }}</td>
+            </tr>
+            <tr class="font-bold">
+              <td>Total</td>
+              <td>{{ game.set.count }}</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   </div>
@@ -19,6 +63,7 @@ import useDoc from '~/use/doc'
 import useLiveDoc from '~/use/liveDoc'
 import useRouter from '~/use/router'
 import TLoader from '~/components/TLoader'
+import { sortBy } from '~/utils'
 
 export default {
   components: {
@@ -45,7 +90,17 @@ export default {
       loading
     }
   },
-  computed: {},
+  computed: {
+    players() {
+      return Object.keys(this.game.players)
+        .map((playerId) => ({
+          id: playerId,
+          nickname: this.game.players[playerId].nickname,
+          place: this.game.places[playerId]
+        }))
+        .sort(sortBy('place'))
+    }
+  },
   async mounted() {
     await this.loadProfile(this.uid)
 
