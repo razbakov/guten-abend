d1d6c2c5b6e790ab9e4a668743a74c8654951cca
Aleksey Razbakov <aleksey@razbakov.com>
2020-04-04 20:11:54 +0200

    add admin route

---
+++ pages/superadmin.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.localStorage.setItem('is_admin', '1')
+    this.$router.replace('/')
+  }
+}
+</script>
