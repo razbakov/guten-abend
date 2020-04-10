75d8c77781f41bab12593aea7960c5b0506ff3ed
Aleksey Razbakov <aleksey@razbakov.com>
2020-04-10 14:07:14 +0200

    campaigns: account filter by event

--- components/TAccountSelector.vue
+++ components/TAccountSelector.vue
@@ -29,6 +29,34 @@
                 None
               </button>
             </div>
+            <div class="inline-block relative">
+              <select
+                v-model="selectedEvent"
+                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              >
+                <option value="">All events</option>
+                <option
+                  v-for="event in events"
+                  :key="event.id"
+                  :value="event.id"
+                  >{{ event.title }} - {{ getDate(event.date) }} at
+                  {{ getTime(event.date) }}</option
+                >
+              </select>
+              <div
+                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+              >
+                <svg
+                  class="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                  />
+                </svg>
+              </div>
+            </div>
           </div>
           <TButton @click="save">Finish</TButton>
         </div>
@@ -37,7 +65,7 @@
           <div
             v-for="item in items"
             :key="item.id"
-            :class="item.selected ? 'border-green-500' : ''"
+            :class="{ 'border-green-500': item.selected }"
             class="p-4 mb-4 card-item border-2 border-white"
             @click="select(item)"
           >
@@ -45,14 +73,14 @@
               class="float-right border w-4 h-4 rounded-full border-gray-500"
               :class="selected[item.id] ? 'bg-green-500 border-green-500' : ''"
             ></div>
-            <div v-if="item.nickname" class="font-bold text-xl mb-2">
-              {{ item.nickname }}
-            </div>
             <div class="font-bold">
               {{ item.name }}
             </div>
             <div>
               {{ item.email }}
+            </div>
+            <div v-if="item.newsletter === 'no'" class="text-red-500">
+              No newsletter
             </div>
           </div>
         </div>
@@ -63,11 +91,11 @@
 
 <script>
 import Vue from 'vue'
-import { computed } from '@vue/composition-api'
+import { computed, ref } from '@vue/composition-api'
 import useAuth from '~/use/auth'
 import useCollection from '~/use/collection'
 import TButton from '~/components/TButton'
-import { sortBy } from '~/utils'
+import { sortBy, getTime, getDate } from '~/utils'
 
 export default {
   components: {
@@ -86,14 +114,38 @@ export default {
   setup() {
     const { isAdmin } = useAuth()
     const { docs } = useCollection('accounts')
+    const { docs: docEvents } = useCollection('meetups')
+    const { docs: participants } = useCollection('participants')
+    const selectedEvent = ref('')
 
     const items = computed(() =>
-      docs.value.filter((item) => !item.hidden).sort(sortBy('-createdAt'))
+      docs.value
+        .map((item) => ({
+          ...item,
+          events: participants.value
+            .filter(
+              (participant) =>
+                participant.uid === item.id && participant.rsvp === 'yes'
+            )
+            .map((participant) => participant.eventId)
+        }))
+        .filter(
+          (item) =>
+            !item.hidden &&
+            (!selectedEvent.value || item.events.includes(selectedEvent.value))
+        )
+        .sort(sortBy('-createdAt'))
     )
+
+    const events = computed(() => docEvents.value.sort(sortBy('-date')))
 
     return {
       isAdmin,
-      items
+      items,
+      events,
+      getTime,
+      getDate,
+      selectedEvent
     }
   },
   computed: {
